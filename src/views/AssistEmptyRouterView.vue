88c445321f619d7a2e6465468a332eef267a2255
silent <silentoopm@163.com>
2020-04-03 10:07:08 +0800

    测试git

--- src/views/AssistEmptyRouterView.vue
+++ src/views/AssistEmptyRouterView.vue
@@ -14,7 +14,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss111" scoped>
 .assist-empty-router-view-container {
   min-height: calc(100vh - 7.25rem);
   width: 100%;
